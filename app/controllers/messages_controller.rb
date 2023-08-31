c34e52c9fb5957875fade50aaca0ffe7307fcfa6
Itswali <luckywali0986@gmail.com>
2023-08-31 16:07:03 +0500

    rubocop

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 # app/controllers/messages_controller.rb
 class MessagesController < ApplicationController
   def random_greeting
-    random_message = Message.order("RANDOM()").first
+    random_message = Message.order('RANDOM()').first
     render json: { greeting: random_message.text }
   end
 end
